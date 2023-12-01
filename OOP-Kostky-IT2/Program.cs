e20bbf8d16c335c321fef6ad78b941ea990e3e7a
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-12-01 10:32:39 +0100

    šest kostek

--- OOP-Kostky-IT2/Program.cs
+++ OOP-Kostky-IT2/Program.cs
@@ -1,9 +1,20 @@
 ﻿using OOP_Kostky_IT2;
 
-Kostka kostka = new Kostka();
+Kostka kostka1 = new Kostka();
+Kostka kostka2 = new Kostka();
+Kostka kostka3 = new Kostka();
+Kostka kostka4 = new Kostka();
+Kostka kostka5 = new Kostka();
+Kostka kostka6 = new Kostka();
+
 
 do
 {
-  kostka.Hod();
-  Console.WriteLine($"Hozeno: {kostka.Hodnota}");
+  kostka1.Hod();
+  kostka2.Hod();
+  kostka3.Hod();
+  kostka4.Hod();
+  kostka5.Hod();
+  kostka6.Hod();
+  Console.WriteLine($"Hozeno: {kostka1.Hodnota} - {kostka2.Hodnota} - {kostka3.Hodnota} - {kostka4.Hodnota} - {kostka5.Hodnota} - {kostka6.Hodnota}");
 }while (Console.ReadKey().Key != ConsoleKey.Escape);
