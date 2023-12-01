e94affb997b32c2a93ec917885d3d6bcfe1fd6e6
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-12-01 10:27:33 +0100

    Přidejte soubory projektu.

---
+++ OOP-Kostky-IT2/Kostka.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Kostky_IT2
+{
+  public class Kostka
+  {
+    private Random random = new Random();
+    public int Hodnota = 1;
+
+    public void Hod()
+    {
+      Hodnota = random.Next(1, 7);
+    }
+  }
+}
---
+++ OOP-Kostky-IT2/Program.cs
@@ -0,0 +1,9 @@
+﻿using OOP_Kostky_IT2;
+
+Kostka kostka = new Kostka();
+
+do
+{
+  kostka.Hod();
+  Console.WriteLine($"Hozeno: {kostka.Hodnota}");
+}while (Console.ReadKey().Key != ConsoleKey.Escape);
